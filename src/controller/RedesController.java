f25dcb2390185bebe6eead17cc9b191bf95a585a
Junior <Junior@DESKTOP-FBQMUOU>
2020-09-08 00:17:03 -0300

    Primeiro Commit

---
+++ src/controller/RedesController.java
@@ -0,0 +1,77 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RedesController {
+	
+	public RedesController () {
+		super();
+	}
+	
+	public void ip(String os) {
+		String testeIp = " "; 
+		
+		 if (os.contains("Windows")) {
+			  testeIp = "ipconfig";
+		  }
+		  else if (os.contains("Linux")) {
+			  testeIp = "ipconfig";
+		  }
+	    try {
+			Process ip = Runtime.getRuntime().exec(testeIp);
+			InputStream fluxo = ip.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+		    String linha = buffer.readLine();
+		    while (linha != null) {
+		    	if(linha.contains("Adaptador Ethernet")){
+		    	System.out.println(linha);}
+		    	else if (linha.contains("IPv4")) {
+		         System.out.println(linha);	}
+		    	
+		    	linha = buffer.readLine();
+		    }
+		    buffer.close();
+		    leitor.close();
+		    fluxo.close();
+		    
+	    } catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void ping(String os) {
+		String ping = " "; 
+		
+		 if (os.contains("Windows")) {
+			  ping = "PING -t10 www.google.com.br";
+		  }
+		  else if (os.contains("Linux")) {
+			  ping = "ipconfig";
+		  }
+	    try {
+			Process ip = Runtime.getRuntime().exec(ping);
+			InputStream fluxo = ip.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+		    String linha = buffer.readLine();
+		    System.out.println("Disparando www.google.com.br [172.217.29.3] com 32 bytes de dados:");
+		    while (linha != null) {
+		    	if (linha.contains("tempo"))
+		    	System.out.println(linha.substring(35,45));
+		    	linha = buffer.readLine();
+		    }
+		    buffer.close();
+		    leitor.close();
+		    fluxo.close();
+		    
+	    } catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,38 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.RedesController;
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		String vai = "Resposta de 172.217.29.3: bytes=32 tempo=5ms TTL=116";
+		System.out.println(vai.substring(35, 45));
+		RedesController redesC = new RedesController();
+		String os = System.getProperty("os.name");
+		int op=0;
+		
+		while(op!=99){
+			op=Integer.parseInt(JOptionPane.showInputDialog("Cadastro\n1 - IP" + "\n2 - Ping" + "\n99 - Sair"));
+
+			switch(op){
+			case 1:
+				redesC.ip(os);
+				break;
+			case 2:
+				redesC.ping(os);
+				break;
+			
+			case 99:
+				JOptionPane.showMessageDialog(null,"Saindo");
+				break;	
+			default:
+				JOptionPane.showMessageDialog(null,"Opção inválida");
+			}
+		}
+	
+	
+	}
+}
